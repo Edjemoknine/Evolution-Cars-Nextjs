ab82680c82f0139c7e7878f43940efde6e0dcb3c
Moknine Elhadj <93493650+Edjemoknine@users.noreply.github.com>
2024-02-03 23:07:54 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className=''>
+        <h1 className='text3Xl font-bold'>Next Ts Starter</h1>
+      </div>
+    </section>
+  )
+}
